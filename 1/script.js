375c55586d2d89c5330ed7c634681b44bbc3a9b1
fpoweredd <130145140+fpoweredd@users.noreply.github.com>
2024-01-29 21:57:48 +0100

    slide show with animation

--- 1/script.js
+++ 1/script.js
@@ -1,21 +1,33 @@
 const nextButton = document.getElementById("next")
 const prevButton = document.getElementById("previous")
 const img = document.getElementById("myimage")
-const imgArray = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg"]
+const imgArray = ["slides/image1.jpg", "slides/image2.jpg", "slides/image3.jpg", "slides/image4.jpg", "slides/image5.jpg"]
 let actualSlide = 0
 
-nextButton.addEventListener("click", function(){
+nextButton.addEventListener("click", function(event){
+    event.preventDefault()
+
     actualSlide++
     if (imgArray.length === actualSlide) {
         actualSlide = 0
     }
-    img.src = imgArray[actualSlide]
+    // img.src = imgArray[actualSlide]
+    let newSlide = document.createElement("img")
+    newSlide.src = imgArray[actualSlide]
+    newSlide.className = "animation"
+    document.getElementById("content").appendChild(newSlide)
 })
 
-prevButton.onclick = function(){
+prevButton.onclick = function(event){
+    event.preventDefault()
     actualSlide--
     if (actualSlide < 0) {
-        actualSlide = imgArray.length - 1;
+        actualSlide = imgArray.length - 1
     }
-    img.src = imgArray[actualSlide]
+    // img.src = imgArray[actualSlide]
+    let newSlide = document.createElement("img")
+    // not a final bcs we can look all time new image in html
+    newSlide.src = imgArray[actualSlide]
+    newSlide.className = "animation"
+    document.getElementById("content").appendChild(newSlide)
 }
